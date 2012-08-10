43f910e06a1d6183a6ff9cea0fb4145e3e05056c
Saif Al-Naib <salnaib@ryerson.ca>
2012-08-10 03:19:03 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlanzApp::Application.config.secret_token = 'e2bc4fb91b24d7ba93fc11ec513082216fc406dab60fe966804a75969594161535d72d6275cb2975860e83297082af570ff788fc32dc5b333704deefbbe82431'
